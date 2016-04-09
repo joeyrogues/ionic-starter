2284775549ad22659c58b7a2644bbd24496adf5b
Joey Rogues <rogues.jonathan.168@gmail.com>
2016-04-09 15:06:47 +0200

    Added $http example

--- www/dist/app.js
+++ www/dist/app.js
@@ -110,9 +110,10 @@ angular.module('myapp')
 
 angular.module('myapp')
 
-.factory('ArticleService', ['$q', function ($q) {
+.factory('ArticleService', ['$q', '$http', function ($q, $http) {
 	var ArticleService = {
 		all: function () {
+			// Mock
 			return $q.when([
 		        { id: 1, title: 'Article 1', description: 'The super article 1' }
 		      , { id: 2, title: 'Article 2', description: 'The super article 2' }
@@ -121,6 +122,12 @@ angular.module('myapp')
 		      , { id: 5, title: 'Article 5', description: 'The super article 5' }
 		      , { id: 6, title: 'Article 6', description: 'The super article 6' }
 		    ])
+
+		    var req = {
+				method : 'GET',
+				url    : 'http://jsonplaceholder.typicode.com/users'
+			}
+			return $http(req)
 		},
 
 		find: function (id) {
